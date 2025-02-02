7e4dca66935612e6eb1f857d02592129ce1c8342
("SUMAN KAR") <145581646+Karsuman4298@users.noreply.github.com>
2025-02-02 22:21:30 +0530

    Create random_points.py

---
+++ random_points.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+x_data=np.random.random(50)
+y_data=np.random.random(50)
+fig=px.scatter(x=x_data,y=y_data)
+fig.show()
+
+
+// OUTPUT :
